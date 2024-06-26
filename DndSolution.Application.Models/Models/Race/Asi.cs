0e9c0a51f7d7b7a2285c46aae4f86009977e4517
hiluk <kestos1243@mail.ru>
2024-06-26 18:39:23 +0200

    Сделал dto расы

---
+++ DndSolution.Application.Models/Models/Race/Asi.cs
@@ -0,0 +1,10 @@
+﻿namespace DndSolution.Application.Models.Models.Race;
+
+public class Asi
+{
+    public int Id { get; init; }
+    
+    public string Stat { get; init; }
+    
+    public string Value { get; init; }
+}
---
+++ DndSolution.Application.Models/Models/Race/Speed.cs
@@ -0,0 +1,10 @@
+﻿namespace DndSolution.Application.Models.Models.Race;
+
+public class Speed
+{
+    public string Id { get; set; }
+    
+    public string Type { get; set; }
+    
+    public int Value { get; set; }
+}
