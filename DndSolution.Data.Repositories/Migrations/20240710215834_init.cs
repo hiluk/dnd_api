0384f3a6aad856e67fd0e44e99ae751617b01d63
hiluk <kestos1243@mail.ru>
2024-07-14 12:14:14 +0200

    Добавил файлы контекста

---
+++ DndSolution.Data.Repositories/Migrations/20240710215834_init.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "character",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    level = table.Column<byte>(type: "smallint", nullable: false),
+                    xp = table.Column<long>(type: "bigint", nullable: false),
+                    race = table.Column<int>(type: "integer", nullable: false),
+                    @class = table.Column<int>(name: "class", type: "integer", nullable: false),
+                    creation_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "class",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    desc = table.Column<string>(type: "text", nullable: false),
+                    hit_dice = table.Column<string>(type: "text", nullable: false),
+                    hp_at_1st_level = table.Column<string>(type: "text", nullable: false),
+                    hp_at_high_levels = table.Column<string>(type: "text", nullable: false),
+                    prof_armor = table.Column<string>(type: "text", nullable: false),
+                    prof_weapon = table.Column<string>(type: "text", nullable: false),
+                    prof_tool = table.Column<string>(type: "text", nullable: false),
+                    prof_saving_throws = table.Column<string>(type: "text", nullable: false),
+                    prof_skills = table.Column<string>(type: "text", nullable: false),
+                    equipment = table.Column<string>(type: "text", nullable: false),
+                    spell_casting_ability = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_class", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "race",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    desc = table.Column<string>(type: "text", nullable: false),
+                    age = table.Column<string>(type: "text", nullable: false),
+                    size = table.Column<string>(type: "text", nullable: false),
+                    language = table.Column<string>(type: "text", nullable: false),
+                    vision = table.Column<string>(type: "text", nullable: false),
+                    traits = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_race", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "stats",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    character_id = table.Column<long>(type: "bigint", nullable: false),
+                    strength = table.Column<int>(type: "integer", nullable: false),
+                    intelligence = table.Column<int>(type: "integer", nullable: false),
+                    wisdom = table.Column<int>(type: "integer", nullable: false),
+                    constitution = table.Column<int>(type: "integer", nullable: false),
+                    dexterity = table.Column<int>(type: "integer", nullable: false),
+                    charisma = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_stats", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_stats_character_character_id",
+                        column: x => x.character_id,
+                        principalTable: "character",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "asi",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    race_id = table.Column<long>(type: "bigint", nullable: false),
+                    stat = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_asi", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_asi_race_race_id",
+                        column: x => x.race_id,
+                        principalTable: "race",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "speed",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    race_id = table.Column<long>(type: "bigint", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_speed", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_speed_race_race_id",
+                        column: x => x.race_id,
+                        principalTable: "race",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_asi_race_id",
+                table: "asi",
+                column: "race_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_speed_race_id",
+                table: "speed",
+                column: "race_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_stats_character_id",
+                table: "stats",
+                column: "character_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "asi");
+
+            migrationBuilder.DropTable(
+                name: "class");
+
+            migrationBuilder.DropTable(
+                name: "speed");
+
+            migrationBuilder.DropTable(
+                name: "stats");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "race");
+
+            migrationBuilder.DropTable(
+                name: "character");
+        }
+    }
+}
